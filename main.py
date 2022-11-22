fdaab4b83715b7bf4e066de67bb4b2c0a38ce11b
cjones1047 <cjmj1047@gmail.com>
2022-11-22 11:35:22 -0600

    question_bank array now filled with object created from Open Trivia DB API, keys accessed in for loop adjusted accordingly

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
-for question in question_data:
-    new_question = Question(question["text"], question["answer"])
+for question in question_data["results"]:
+    new_question = Question(question["question"], question["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
